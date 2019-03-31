859fa859239b44fdfc48414ddb07ff88d7c99291
PauBadiaM <44896790+PauBadiaM@users.noreply.github.com>
2019-03-31 19:09:48 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from distutils.core import setup
 
 setup(
-   name='4SMacroBuilder',
+   name='MB',
    version='0.0.1',
-   description='MacrocomplexBuilder is a python program designed to 	generate macrocomplex structures from simple pair interactions',
+   description='MacroBuilder is a python program designed to 	generate macrocomplex structures from simple pair interactions',
    long_description=open('README.md').read(),
-   author='B. Pau, C. Altair, S. Natàlia',
+   author='B. Pau, C. Altaïr, S. Natàlia',
    url='https://github.com/Altairch95/4SMacroBuilder',
-   packages=['4SMacroBuilder'],  
+   packages=['MB'],  
    install_requires=['biopython >= 1.73.0',
 		                 'numpy >= 1.16.1'], 
    license='LICENSE.txt',
@@ -17,6 +17,5 @@ setup(
                 "Programming Language :: Python :: 3",
 	              "License :: OSI Approved :: MIT License",
                 "Operating System :: OS Independent"],
-    scripts=[
-              "4SMacroBuilder/MBlauncher.py" ],
+    scripts=["MB/MBlauncher.py" , "MB/MB_GUI.py"]
 )
